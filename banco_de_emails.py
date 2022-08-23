717d7cc58fba868acbe29bcec1c8a6d83a7e438b
Vinicius de Souza Santos <69911251+gladistory@users.noreply.github.com>
2022-08-23 14:15:41 -0300

    Update banco_de_emails.py

--- banco_de_emails.py
+++ banco_de_emails.py
@@ -1,22 +1,8 @@
-'''email = [
-         'desouza850@gmail.com', 'gabrielbandeira.advogado@gmail.com',
-         'gralha@gralhaimoveis.com.br', 'tiago@portodailhaimoveis.com.br', 'eng_rends@hotmail.com',
-         'daniel@dimasconstrucoes.com.br',
-         'gldellajustina@gmail.com', 'mara@firstsa.com.br', 'grego2008@gmail.com', 'ornelas10@gmail.com',
-         'aristidespanstein.eng@gmail.com', 'ricardo@rgt.com.br', 'rubens@projetix.com.br', 'silvio@haitoyota.com.br',
-         'adrianokoerich@hotmail.com', 'giaco@giacomelli.com.br', 'rogernmoraes@gmail.com', 'Jucaoliveira@icloud.com',
-         'jc_becker@hotmail.com', 'zulmarcoutinhojr@hotmail.com', 'antoniobiasi13@gmail.com', 'ednoheck@gmail.com',
-         'danielsalama2000@hotmail.com', 'danielvalentee@hotmail.com', 'valdirantoniomachado@gmail.com',
-         'godofredosteinbach@hotmail.com', 'gmsautomoveis@hotmail.com', 'neltonluis@yahoo.com.br',
-         'neltonluis@yahoo.com.br', 'comercial@lumisconstrutora.com.br', 'comercial.almconstrutora@gmail.com',
-         'comercial@construtoralocks.com.br', 'vendasjurere@gmail.com', 'vos@gsantafe.com.br',
-         'roberto@tyucoimoveis.com.br', 'Aaalex035@gmail.com', 'lukairie18@gmail.com', 'alinevelhos@gmail.com',
-         'carlos-chanca@hotmail.com', 'matheus.okxjia@gmail.com', 'dusanches.es33@gmail.com',
-         'Edineiabester@hotmail.com']
+'''email = ['exemplo1@gmail.com', 'exemplo2@gmail.com' ...]
 '''
 
-email = ['desouza850@gmail.com']
 
+# Para teste
+email = ['seu email']
 
-print(len(email))
 
