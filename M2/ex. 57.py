a5fa57c28136e809f7f2d79ddf6638925a58144f
Vinicius de Souza Santos <69911251+gladistory@users.noreply.github.com>
2025-02-16 19:11:00 -0300

    Delete M2 directory

--- M2/ex. 57.py
+++
@@ -1,4 +0,0 @@
-sexo = str(input('informe seu sexo: [M/F]')).strip().upper()[0]
-while sexo not in 'MmFf':
-      sexo = str(input('dado inválido, informe seu sexo:')).strip().upper()[0]
-print('Sexo {} registrado com sucesso'.format(sexo))
--- M2/ex.51.py
+++
@@ -1,7 +0,0 @@
-# P.A
-n = int(input('primeiro termo:'))
-r = int(input('razão:'))
-decimo = n + (10 - 1) * r
-for c in range(n, decimo, r):
-    print(c, end=' ♠ ')
-print('ACABOU')
